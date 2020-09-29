95317f0443782b9ff56ceda229cdd04b26fca38d
haavardmk <72075895+haavardmk@users.noreply.github.com>
2020-09-29 13:35:38 +0200

    Mal for server

---
+++ Maler for server og client/Mal for server.py
@@ -0,0 +1,12 @@
+# A Simple TCP server, used as a warm-up exercise for assignment A3
+
+def run_server():
+    # TODO - implement the logic of the server, according to the protocol.
+    # Take a look at the tutorial to understand the basic blocks: creating a listening socket,
+    # accepting the next client connection, sending and receiving messages and closing the connection
+    print("Starting TCP server...")
+
+
+# Main entrypoint of the script
+if __name__ == '__main__':
+    run_server()
