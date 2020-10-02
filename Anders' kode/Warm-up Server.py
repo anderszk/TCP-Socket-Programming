2fd0680ce73887ed4e39e60504c53a47b42d70c0
Anders Kristensen <71840284+anderszk@users.noreply.github.com>
2020-10-02 16:10:44 +0200

    Add files via upload

--- Anders' kode/Warm-up Server.py
+++ Anders' kode/Warm-up Server.py
@@ -2,63 +2,69 @@ from socket import * #Importerer Socket Modulen
 
 global connection_socket
 
-def start_server():
-    global connection_socket
-    server_socket = socket(AF_INET, SOCK_STREAM)  # Connecter til sockets hhv. internett og TCP
-    server_socket.bind(("", 666)) #Henter IP-addresse og Porten fra clienten
-    server_socket.listen(1) #1 queue
-    print("Server ready for client connections") #Forteller at serveren er klar for å ta imot brukere
-    connection_socket, client_address = server_socket.accept() #Henter IP-addresse og Porten fra clienten
-    print("Client connected from: ",client_address) #Printer sistnevnte
+def start_server(): #Function to start the server and let the client connects
+    global connection_socket #Global type makes it available for use in functions as a local variable
+    server_socket = socket(AF_INET, SOCK_STREAM)  # Connects to sockets: internett and TCP
+    server_socket.bind(("", 6666)) #Binds the client to the server
+    server_socket.listen(1) #1 spot in queue
+    print("Server ready for client connections")
+    connection_socket, client_address = server_socket.accept() #Accepts the 3-way handshake
+    print("Client connected from: ",client_address) #Prints clients stats
 
 def read_request():
     try:
-        global connection_socket
-        request_client = connection_socket.recv(1000).decode()
-        request = request_client.strip("\n")
+        global connection_socket #Global type makes it available for use in functions as a local variable
+        request_client = connection_socket.recv(1000).decode() #Receives the data from the client
+        request = request_client.strip("\n") #Strips newline
         print("Message from server: ",request)
-        return request #Returnerer liste med
-    except ConnectionResetError:
+        return request #Returns the list with data from the client
+    except ConnectionResetError: #Excepts the connectionreseterror
         print("oops.. something happened")
 
 
-def send_response(total):
+def send_response(total): #Function to send a respons back to the client
+    global connection_socket #Global type makes it available for use in functions as a local variable
+    send = str(total) #Changes the datatype from integer to string
+    response_server = connection_socket.send(send.encode()) #Sends and encodes the message as byte-array
+    print("Message sent to Client: ",send)
+
+
+def adder(response): #Function to both determine if the response was integers and to detect errors, will always return total
     try:
-        global connection_socket
-        send = str(total)
-        response_server = connection_socket.send(send.encode())
-        print("Message sent to Client")
-
-
-def server_function():
-    request = read_request()
-    total = adder(request)
-    print("The two integers added sums to:",total)
-    send_response(total)
-    return total
-
-def adder(response):
-
-    try:
-        total = eval(response)
+        total = eval(response) #Uses the eval()-function to determine the integers and mathematical operations.
+        #I had problems with splitting the integers and operators, explained in README.
         return total
-    except (NameError):
+    except (NameError): #If the input is not accepted in the eval()-function. In other words, the request from the server was not integers
         total = "Wrong format, could not add"
         return total
-    except (TypeError):
+    except (TypeError): #The same as above?
         total = "Wrong format, could not add"
         return total
-    except SyntaxError:
+    except SyntaxError: #The error that will occur if it exits without finishing all the codeblocks
         print("Client disconnected from Server!")
         return False
+
+
+def server_function(): #Function to assemble all the other functions #habit from arduino
+    request = read_request() #Reads the request from the client
+    total = adder(request) #Checks and add the data from the server
+    if total == False:
+        return False
+    else:
+        send_response(total) #Sends the response to the client
+        return True
+
+
+
+
 
 
 if __name__ == "__main__": #Prox
     start_server()
     while True:
         server_function()
-        if adder("nothing") == False:
-            break;
+        if server_function() == False:
+            break
     print("Goodbye!")
 
 
--- Anders' kode/Warm-up_exercise_final.py
+++ Anders' kode/Warm-up_exercise_final.py
@@ -1,36 +1,34 @@
 # A Simple TCP client, used as a warm-up exercise for socket programming assignment.
 # Course IELEx2001, NTNU
 
-import random
-import time
-from socket import *
-import os
+import random #Importing the random module to get random integers
+import time #Importing the time module
+from socket import * #Importing the socket module the use the commands and library
 
 # Hostname of the server and TCP port number to use
-HOST = input("Enter the name of the server you want to join: ")  #"datakomm.work"
-PORT = int(input("Enter your port here: "))  #1301
+HOST = input("Enter the name of the server you want to join: ")  #User enter the hostname of the server, in this case "datakomm.work"
+PORT = int(input("Enter your port here: "))  #User enter the port of the socket, in this case 1301
 
 # The socket object (connection to the server and data exchange will happen using this variable)
-client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket = socket(AF_INET, SOCK_STREAM) #This needs to be a global variable because its used in several functions
 
 
-def connect_to_server(HOST, PORT):
-    global client_socket
+def connect_to_server(HOST, PORT): #Function for the client to connect to the server, with input entered then the program is started.
+    global client_socket #includes a global variable in the function
     try:
         client_socket.connect((HOST, PORT))  # Connecter til Sockets med inputs som ble spurt om tidligere
-        client_socket.settimeout(3)
-        return True
+        client_socket.settimeout(3) #Gives an error if the client fails to connect to the server within 3 seconds
+        return True #Return the boolean value True if it manages to connect
     except:
-        return False
+        return False #Return False if the client fails to connect to the server
 
 
-def close_connection():
-    global client_socket
-    try:
-        client_socket.close()  # Stenger/Avslutter tilkoblingen
-        ("CLOSED")
+def close_connection(): #Function to disconnect the client from the server.
+    global client_socket #includes a global variable in the function
+    try: #Tries to do the following
+        client_socket.close() #Command from the socket module to disconnect
         return True
-    except IOError:
+    except IOError: #If the following error occurs, it prevents the error the stop the program, but will do this instead:
         print("Something went wrong")
         return False
 
@@ -38,17 +36,17 @@ def close_connection():
 
 
 def send_request_to_server(request):
-    global client_socket
-    request_strip = request.strip("+")
-    split_send = request_strip.split()
+    global client_socket #includes a global variable in the function
+    #request_strip = request.strip("+")
+    split_send = request.split()
     try:
-        if any('game' and 'over' in s for s in split_send) == True:
-            close_connection()
+        if any('game' and 'over' in s for s in split_send) == True: #Uses the find function to decide if the list contains certain words
+            close_connection() #Triggers the close function
             print("You typed: Game over, you have been disconnected from the server")
             return False
         else:
-            for u in split_send:
-                client_socket.send(u.encode())
+            for u in split_send: #Sends the message to the server
+                client_socket.send(u.encode()) #Encodes the message to a byte.array
             return True
     except OSError:
         "Youre not connected to the server"
@@ -56,18 +54,18 @@ def send_request_to_server(request):
 
 
 def read_response_from_server():
-    global client_socket
-    response_encoded = client_socket.recv(1000)
-    response_decoded = response_encoded.decode()
-    response = response_decoded.strip("\n")
+    global client_socket #includes a global variable in the function
+    response_encoded = client_socket.recv(1000) #Receive-function, the argument is how many bits you want to receive
+    response_decoded = response_encoded.decode() #Decodes the byte-array to a string
+    response = response_decoded.strip("\n") #Strip-function that removes (argument) from the string
 
-    if response is None:
+    if response is None: #Returns false if the client does not receive any messages from the server
         return False
     else:
-        return response
+        return response #If it does, it will return the response
 
 
-def run_client_tests():
+def run_client_tests(): #Function to test the different functions.
     """
     Runs different "test scenarios" from the TCP client side
     :return: String message that represents the result of the operation
@@ -89,16 +87,66 @@ def run_client_tests():
     if response is None:
         return "ERROR: Failed to receive server's response!"
 
+    time.sleep(1)  # Delay added to analyze the response and prints
     print("Server responded with: ", response)
+
+
+
+#Same as the function over, just to check consistency of the request/response
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    request = str(a) + "+" + str(b)
+
+    if not send_request_to_server(request):
+        return "ERROR: Failed to send valid message to server!"
+
+    print("Sent ", request, " to server")
+    response = read_response_from_server()
+    if response is None:
+        return "ERROR: Failed to receive server's response!"
+
+    time.sleep(1)  # Delay added to analyze the response and prints
+    print("Server responded with: ", response)
+
+
+
+
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    request = str(a) + "+" + str(b)
+
+    if not send_request_to_server(request):
+        return "ERROR: Failed to send valid message to server!"
+
+    print("Sent ", request, " to server")
+    response = read_response_from_server()
+    if response is None:
+        return "ERROR: Failed to receive server's response!"
+
+    time.sleep(1)  # Delay added to analyze the response and prints
+    print("Server responded with: ", response)
+
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+
+
+
     seconds_to_sleep = 2 + random.randint(0, 5)
     print("Sleeping %i seconds to allow simulate long client-server connection..." % seconds_to_sleep)
     time.sleep(seconds_to_sleep)
 
     print("Pause done")
 
+    time.sleep(1) #Delay added to analyze the response and prints
+
     request = "bla+bla"
     if not send_request_to_server(request):
         return "ERROR: Failed to send invalid message to server!"
+
+    time.sleep(1) #Delay added to analyze the response and prints
 
     print("Sent " + request + " to server")
     response = read_response_from_server()
@@ -107,12 +155,18 @@ def run_client_tests():
 
     print("Server responded with: ", response)
 
+    time.sleep(1) #Delay added to analyze the response and prints
+
     send_request_to_server("game over")
+
+    time.sleep(1) #Delay added to analyze the response and prints
 
     print("Game over, connection closed")
     # When the connection is closed, try to send one more message. It should fail.
     if send_request_to_server("2+2"):
         return "ERROR: sending a message after closing the connection did not fail!"
+
+    time.sleep(2) #Delay added to analyze the response and prints
 
     print("Sending another message after closing the connection failed as expected")
     return "Simple TCP client finished"
